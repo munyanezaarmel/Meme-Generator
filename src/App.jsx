de14ba2571325902dd25bd761a44115bb96badd2
Munyaneza Armel <munyaarmel61@gmail.com>
2023-01-26 13:40:14 +0200

    remove body

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './index.css'
 import Header from "./Components/Header"
 import Form from './Components/Form'
-import Body from './Components/Body'
  const App=()=>{
     return(
         <div className='container'>
        <Header/>
        <Form />
-       <Body/>
         </div>
     )
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import body from '../images/body.png';
 const Form =()=>{
     return (
         <div className='form-container'>
      <div className="input">
-        <input type="text" className='input1' />
-        <input type="text" className='input2' />
+        <input type="text" className='input1' placeholder='enter top text' />
+        <input type="text" className='input2' placeholder='enter bottom text' />
      </div>
      <button className='submit'> Get a new image</button>
+     <div className="meme-image-container">
+       <img src={body} alt="meme  of meme" className="meme-image" />
+        </div>
         </div>
     )
 }
