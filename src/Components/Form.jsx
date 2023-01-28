2427b49f59a3a7ed1d88d2824ab3a5368e75584d
Munyaneza Armel <munyaarmel61@gmail.com>
2023-01-28 09:34:25 +0200

    fetching api

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import body from '../images/body.png';
+import data from '../data';
 const Form =()=>{
+    function getMemeImage(){
+     console.log(data)
+
+    }
     return (
         <div className='form-container'>
      <div className="input">
         <input type="text" className='input1' placeholder='enter top text' />
         <input type="text" className='input2' placeholder='enter bottom text' />
      </div>
-     <button className='submit'> Get a new image</button>
+     <button className='submit' onClick={getMemeImage()}> Get a new image</button>
      <div className="meme-image-container">
        <img src={body} alt="meme  of meme" className="meme-image" />
         </div>
