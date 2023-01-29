7680633592bc87b5b574f7608c867f2dd30df5bd
Munyaneza Armel <munyaarmel61@gmail.com>
2023-01-29 16:16:48 +0200

    selecting random meme

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import body from '../images/body.png';
 import memesData from '../memesData.js';
+import { useState } from 'react';
 const Form =()=>{
+    const[memeImage,setMemeImage]=useState("")
+
     function getMemeImage(){
     const memeData=memesData.data.memes
     const randomNumber=Math.floor(Math.random()*memeData.length)
-    const {url}=memeData[randomNumber]
-    console.log(url)
+    const url=memeData[randomNumber].url
+    setMemeImage(url)
 
     }
     return (
@@ -15,9 +17,9 @@ const Form =()=>{
         <input type="text" className='input1' placeholder='enter top text' />
         <input type="text" className='input2' placeholder='enter bottom text' />
      </div>
-     <button className='submit' onClick={getMemeImage()}> Get a new image</button>
+     <button className='submit' onClick={getMemeImage}> Get a new image</button>
      <div className="meme-image-container">
-       <img src={body} alt="meme  of meme" className="meme-image" />
+       <img src={memeImage} alt="meme  of meme" className="meme-image" />
         </div>
         </div>
     )
