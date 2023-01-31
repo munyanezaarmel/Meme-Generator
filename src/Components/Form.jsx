f857afd88f75153020a0602839fd21b44bcf6ded
Munyaneza Armel <munyaarmel61@gmail.com>
2023-01-31 17:25:03 +0200

    added top text bottom text

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -8,25 +8,39 @@ const Form =()=>{
         randomImage:"https://i.imgflip.com/39t1o.jpg"
     })
 const [allMemeImage,setAllMemeImage]=useState(memesData)
-    function getMemeImage(){
+const [messages,setMessages]=useState(["a","b"])
+    function getMemeImage(event){
+        const{name,value}=event.target
     const memesArray=memesData.data.memes
     const randomNumber=Math.floor(Math.random()*memesArray.length)
     const {url}=memesArray[randomNumber]
     setMeme((meme)=>{
         return {
-... meme, randomImage:url
+... meme, randomImage:url,
+          [name]:value
         }
     })
+    }
+    function handleChange(event){
+        const{name,value}=event.target
+        setMeme((meme)=>{
+            return {
+    ... meme,
+              [name]:value
+            }
+        })
     }
     return (
         <div className='form-container'>
      <div className="input">
-        <input type="text" className='input1' placeholder='enter top text' />
-        <input type="text" className='input2' placeholder='enter bottom text' />
+        <input type="text" className='input1' placeholder='enter top text'onChange={handleChange} name="topText" value={meme.topText}/>
+        <input type="text" className='input2' placeholder='enter bottom text' onChange={handleChange} name="bottomText" value={meme.bottomText} />
      </div>
      <button className='submit' onClick={getMemeImage}> Get a new image</button>
      <div className="meme-image-container">
        <img src={meme.randomImage} alt="meme  of meme" className="meme-image" />
+       <h2 className='meme-text-top'>{meme.topText}</h2>
+       <h2 className='meme-text-bottom'>{meme.bottomText}</h2>
         </div>
         </div>
     )
